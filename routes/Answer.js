2bf56f03fd23bde2efd7b780087decbcd412bf09
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2023-12-23 17:28:28 +0530

    Route exports

--- routes/Answer.js
+++ routes/Answer.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { listAnswer, addAnswer } = require("../controller/Answer");
+const { cookieauthorization } = require("../middleware/CookieAuth");
+const { route } = require("./Question");
+
+router.get("/list", cookieauthorization, listAnswer);
+router.get("/add", cookieauthorization, addAnswer);
+
+export default router;
--- routes/DownVote.js
+++ routes/DownVote.js
@@ -1,3 +1,10 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
+
+const { AddDownVote, DownvoteCount } = require("../controller/Upvote");
+const { cookieauthorization } = require("../middleware/CookieAuth");
+
+router.get("/total", cookieauthorization, DownvoteCount);
+router.post("/add", cookieauthorization, AddDownVote);
+
+export default router;
