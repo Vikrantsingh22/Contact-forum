5b19124d1ca480fee4070bb0bab8c6cae8d1b052
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2023-12-23 17:36:21 +0530

    Configured App.js

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 connectdb();
+
+const UserRouter = require("./routes/User");
+const QuestionRouter = require("./routes/Question");
+const AnswerRouter = require("./routes/Answer");
+const UpvoteRouter = require("./routes/UpVote");
+const DownvoteRouter = require("./routes/DownVote");
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("SERVER IS RUNNING on home page");
 });
+app.use("/User", UserRouter);
+app.use("/Question", QuestionRouter);
+app.use("/Answer", AnswerRouter);
+app.use("/upvote", UpvoteRouter);
+app.use("/downvote", DownvoteRouter);
 app.listen(port, () => {
   console.log("server is live");
 });
