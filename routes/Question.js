94e6ff3b62b474646dd4544ba4fc579aa30586bb
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2023-12-23 17:36:54 +0530

    Minor mistake

--- routes/Question.js
+++ routes/Question.js
@@ -7,4 +7,4 @@ const { cookieauthorization } = require("../middleware/CookieAuth");
 router.get("/list", cookieauthorization, listQuestion);
 router.post("/add", cookieauthorization, addQuestion);
 
-module.exports = router;
+export default router;
