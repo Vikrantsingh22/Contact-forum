fad9129c9d4cf3ece5291e45d398326db63a377a
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2023-12-23 14:23:36 +0530

    List Questions

--- controller/Question.js
+++ controller/Question.js
@@ -9,7 +9,39 @@ async function addQuestion(req, res) {
       user_id,
     });
     console.log(newQuestion.id);
+    res.status(200);
   } catch (e) {
     console.log(`Question do not get added ${e}`);
+  }
+}
+
+async function listQuestion(req, res) {
+  try {
+    const questionString = req.body;
+    let filter;
+    if (questionString != null) {
+      filter = {
+        questionString: {
+          $regex: questionString,
+        },
+      };
+    } else {
+      filter = 0;
+    }
+
+    const { limit, page } = req.query;
+    const QuestionList = await QuestionModel.find(filter)
+      .limit((limit = 0))
+      .skip((page - 1) * limit);
+    const count = await QuestionModel.count();
+    const totalPages = Math.ceil(count / limit);
+    const Question = { totalPages, data: QuestionList };
+    if (Question) {
+      res.json({
+        res: Question,
+      });
+    }
+  } catch (e) {
+    console.log(`Error occured ${e}`);
   }
 }
